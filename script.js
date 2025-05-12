edb09413d6437a7374e989379d5f3b2437ab4b02
Benjamin Isaac <benisaac03@gmail.com>
2025-05-11 21:40:01 -0500

    Add CSS to make projects readable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function  makeProject(title, dueDate, toDos){
-    let numToDosRemaining = toDos.length;
+function  makeProject(title, dueDate, toDoList, notes){
+    let numToDosRemaining = toDoList.length;
 
-    return {title, dueDate, toDos, numToDosRemaining}
+    return {title, dueDate, toDoList, numToDosRemaining, notes}
 }
 
 function makeToDo(title, dueDate, priority, notes, project){
@@ -17,6 +17,7 @@ function newProjectDOM(project){
 
     const newProject = document.createElement('div');
     newProject.id = projectClass;
+    newProject.classList.add('projectEntry');
 
     const title = document.createElement('p');
     title.textContent = project.title;
@@ -24,8 +25,8 @@ function newProjectDOM(project){
     const dueDate = document.createElement('p');
     dueDate.textContent = project.dueDate;
     dueDate.classList.add('dueDate', 'project', projectClass);
-    const toDos = document.createElement('div');
-    toDos.classList.add('toDos', 'project', projectClass);
+    const toDoList = document.createElement('div');
+    toDoList.classList.add('toDoList', 'project', projectClass);
     const numToDosRemaining = document.createElement('p');
     numToDosRemaining.textContent = project.numToDosRemaining;
     numToDosRemaining.classList.add('numToDosRemaining', 'project', projectClass);
@@ -34,55 +35,61 @@ function newProjectDOM(project){
     notes.classList.add('notes', 'project', projectClass);
 
     const addToDoButton = document.createElement('button');
-    addToDoButton.classList.add('addTodo', projectClass)
+    addToDoButton.classList.add('addToDo', projectClass)
     addToDoButton.textContent = 'Add task';
     
     newProject.appendChild(title);
     newProject.appendChild(dueDate);
-    newProject.appendChild(toDos);
+    newProject.appendChild(toDoList);
+    newProject.appendChild(numToDosRemaining);
     newProject.appendChild(notes);
     newProject.appendChild(addToDoButton);
     projectDiv.appendChild(newProject);
 
-    for (let i = 0; i < project.toDos.length; i++){
-        newToDoDOM(project.toDos[i]);
+    for (let i = 0; i < project.toDoList.length; i++){
+        newToDoDOM(project.toDoList[i]);
     }
 }
 
 function newToDoDOM(toDo){
-    let toDoListClass = toDo.project + " toDos";
+    let toDoListClass = toDo.project + " toDoList";
     const projectToDoList = document.getElementsByClassName(toDoListClass)[0];
 
     const numToDos = projectToDoList.childElementCount;
-    const toDoClass = 'ToDo'+numToDos;
+    const toDoClass = 'toDo'+numToDos;
     const newToDo = document.createElement('div');
+    newToDo.classList.add('toDoEntry');
     newToDo.id = toDoClass;
 
-    const title = document.createElement('p');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox', 'toDo', toDoClass);
+    const title = document.createElement('span');
     title.textContent = toDo.title;
     title.classList.add('title', 'toDo', toDoClass);
-    const dueDate = document.createElement('p');
+    const dueDate = document.createElement('span');
     dueDate.textContent = toDo.dueDate;
     dueDate.classList.add('dueDate', 'toDo', toDoClass);
-    const priority = document.createElement('p');
+    const priority = document.createElement('span');
     priority.textContent = toDo.priority;
     priority.classList.add('priority', 'toDo', toDoClass);
-    const notes = document.createElement('p');
+    const notes = document.createElement('span');
     notes.textContent = toDo.notes;
     notes.classList.add('notes', 'toDo', toDoClass);
-    const isFinished = document.createElement('p');
-    isFinished.textContent = toDo.isFinished;
-    isFinished.classList.add('isFinished', 'toDo', toDoClass);
 
+    newToDo.append(checkbox)
     newToDo.appendChild(title);
     newToDo.appendChild(dueDate);
     newToDo.appendChild(priority);
     newToDo.appendChild(notes);
-    newToDo.appendChild(isFinished);
     projectToDoList.appendChild(newToDo);
 }
 
 myToDos = [];
 myToDos.push(makeToDo('Clean', 'tomorrow', 'high', 'NA', 'project0'))
-project1 = makeProject('Best Project', 'May', myToDos);
+myToDos.push(makeToDo('Clean', 'tomorrow', 'high', 'NA', 'project0'))
+myToDos.push(makeToDo('Clean', 'tomorrow', 'high', 'NA', 'project0'))
+myToDos.push(makeToDo('Clean', 'tomorrow', 'high', 'NA', 'project0'))
+
+project1 = makeProject('Best Project', 'May', myToDos, 'This is the best project ever.');
 newProjectDOM(project1);
